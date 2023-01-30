0e83ce54b15e39c6501599eee9371365832191d6
Adebayo Jagunmolu <hardebahyho@gmail.com>
2023-01-30 09:26:29 +0100

    Fixed social links

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -13,11 +13,19 @@
 	<div>
 		<span class="footer-title">Social</span>
 		<div class="grid grid-flow-col gap-4">
-			<a href="#" target="_blank">
+			<a href="https://www.twitter.com/Itscoastng" target="_blank" rel="noreferrer">
 				<TwitterIcon />
 			</a>
-			<a href="#" target="_blank"><YoutubeIcon /></a>
-			<a href="#" target="_blank"><FacebookIcon /></a>
+			<a
+				href="https://www.youtube.com/channel/UCRvVfL6orUrCEDvlKswWXSw"
+				target="_blank"
+				rel="noreferrer"><YoutubeIcon /></a
+			>
+			<a
+				href="https://www.facebook.com/Coast-Ng-Resources-108244488554161/"
+				target="_blank"
+				rel="noreferrer"><FacebookIcon /></a
+			>
 		</div>
 	</div>
 </footer>
